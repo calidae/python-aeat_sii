e6c6e9ea9f5ee45c50225d799359acf4ca7095e0
Dani Möller <daniel@calidae.com>
2017-07-05 13:28:01 +0200

    Bump version 0.2.4 -> 0.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_require = [
 
 setup(
     name='pyAEATsii',
-    version='0.2.4',
+    version='0.2.5',
     description='A python wrapper for the AEAT SII webservices',
     author="Calidae S.L.",
     author_email="dev@calidae.com",
